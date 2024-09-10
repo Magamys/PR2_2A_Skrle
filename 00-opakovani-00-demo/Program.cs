d33c7756e66b3eeeb93b0410f60231860370e1c1
Stepan SKRLE <stepan.skrle@email.cz>
2024-09-10 10:14:07 +0200

    Odevzdání demo projektu

---
+++ 00-opakovani-00-demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _00_opakovani_00_demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
