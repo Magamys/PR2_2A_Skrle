2da93ef6b1c7debf86987e7a37587457e8ea64a7
Magamys <stepan.skrle@email.cz>
2024-10-01 09:17:36 +0200

    Odevzdání testu

---
+++ 00-Rev-01-poslední-dlouhe/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _00_Rev_01_poslední_dlouhe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pole = { "Karel", "Jiřina", "Hnidoslav", "Lukáš" };
+            
+
+            Console.WriteLine(PosledniDlouhe(pole, 6));
+            Console.WriteLine(PosledniDlouhe(pole, 2));
+            Console.WriteLine(PosledniDlouhe(pole, 12));
+        }
+
+        public static string PosledniDlouhe(string[] pole, int limit)
+        {
+            string nejdelsi = "";
+
+            for(int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i].Length >= limit)
+                    nejdelsi = pole[i];
+                
+            }
+
+            return nejdelsi;
+        }
+    }
+}
---
+++ 00-Rev-02-prumer-lichych/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _00_Rev_02_prumer_lichych
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string vklad;
+            int num;
+            int celkem = 0;
+            int i = 0;
+            
+
+            Console.WriteLine("Zadej celé číslo, nebo stop: ");
+
+            while (true)
+            {
+                vklad = Console.ReadLine();
+
+                if (vklad == "stop")
+                    break;
+
+                int.TryParse(vklad, out num);
+
+                if (num > 0 && num % 2 != 0)
+                {
+                    celkem += num;
+                    i++;
+                }
+                    
+            }
+
+            double prumer = celkem / i;
+
+            if(i == 0)
+                Console.WriteLine("nebylo vloženo žádné kladné liché číslo");
+            else
+                Console.WriteLine(prumer);
+        }
+    }
+}
